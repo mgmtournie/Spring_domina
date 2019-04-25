8191cbf7e8474c75329ce36cfd98bda19746db5b
Tournie Mathilde <mgtournie@gmail.com>
2019-04-25 14:56:35 +0200

    DAO

--- src/main/java/com/DAO/CapteurDAO.java
+++ src/main/java/com/DAO/CapteurDAO.java
@@ -1,12 +1,19 @@
 package com.DAO;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.entity.Capteur;
 
 @Repository
-public interface CapteurDAO {
-	 public Capteur foundByIdCapteur(int idCapteur);
+public interface CapteurDAO extends JpaRepository<Capteur, Integer>{
+	 public Capteur findByIdCapteur (int idCapteur);
+	 
+	 public  List<Capteur> findByPiece (int idPiece); 
+	 
+	 public List<Capteur> findByType (String type); 
 	 
 	 
 
--- src/main/java/com/DAO/InstallationDAO.java
+++ src/main/java/com/DAO/InstallationDAO.java
@@ -2,16 +2,25 @@ package com.DAO;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.entity.Installation;
+import com.entity.Piece;
 
 @Repository
-public interface InstallationDAO {
-	public List<Installation> foundByType(int type);
+public interface InstallationDAO  extends JpaRepository<Installation, Double>{
+	// findbynom, find by etat
+	public List<Installation> findByNomInstal(String type);
 	
-	public List<Installation> foundByPiece(int idPiece);
+	public List<Installation> findByPiece(int idPiece);
 	
-	public Installation foundById(int idInstall); 
+	public Installation findById(int idInstall); 
+	
+	public  List<Installation> findByProgrammes (int idProg); 
+	
+	public  Installation findByEtat (boolean etat); 
+	
+	
 
 }
